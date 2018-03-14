9a3844dc771aae4d361c6de911e0bf5002041b67
Aleksandra <salamanova@phystech.edu>
2018-03-14 23:01:56 +0300

    Исправления в дз

--- week2/sort/src/sort/Sort.java
+++ week2/sort/src/sort/Sort.java
@@ -3,9 +3,6 @@ package sort;
 public class Sort {
 
     public static void bubbleSortForward(int[] arr, int[] iterationNum){
-    /*Внешний цикл каждый раз сокращает фрагмент массива, 
-      так как внутренний цикл каждый раз ставит в конец
-      фрагмента максимальный элемент*/   
         for(int i = arr.length-1 ; i > 0 ; i--){
             for(int j = 0 ; j < i ; j++){
             /*Сравниваем элементы попарно, 
